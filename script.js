81149bb342fd6169478fcc73f8210ccefd0784c6
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-11-07 23:24:59 -0700

    added new functions,variables

--- script.js
+++ script.js
@@ -1,84 +1,167 @@
-questionNumber = 0;
-correctCount = 0;
-answers = "";
+const startBtn = document.querySelector("#start");
+const startPrompt = document.querySelector("#start-prompt");
+const questionBox = document.querySelector("#questionBox");
+const questionText = document.querySelector("#question-text");
+const answersBox = document.querySelector("#answers");
+const timer = document.querySelector("#timer");
+const formBox = document.querySelector("#form");
+let time = document.querySelector("#time");
+let finalScore = document.querySelector("#final-score");
+let score = 0;
+let correctScore = 0;
+let fine = 10;
+let timeTotal = 75;
+time.textContent = `${timeTotal} seconds left`;
 
-questionN = "";
-choiceA = "";
-choiceB = "";
-choiceC = "";
-let choices = "";
-let choice = "";
-
-const quizQuestions = [
+const questions = [
   {
-    question: "30+40",
-    a: "20",
-    b: "40",
-    c: "70",
-    answer: "C",
+    questionText: "Commonly used data types DO NOT include:",
+    answers: ["strings", "booleans", "alerts", "numbers"],
+    correctIndex: 2,
   },
   {
-    question: "8/4+2",
-    a: "1.3333",
-    b: "4",
-    c: "3",
-    answer: "B",
+    questionText:
+      "The condition in an if / else statement is enclosed within ____.",
+    answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correctIndex: 2,
+  },
+  {
+    questionText: "Arrays in Javascript can be used to store ____.",
+    answers: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    correctIndex: 3,
+  },
+  {
+    questionText:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    correctIndex: 2,
+  },
+  {
+    questionText:
+      "A very useful tool for used during development and debugging for printing content to the debugger is:",
+    answers: ["Javascript", "terminal / bash", "for loops", "console log"],
+    correctIndex: 3,
   },
 ];
 
-function getElement(x) {
-  return document.getElementById(x);
+let questionInd = 0;
+
+startBtn.addEventListener("click", startClick);
+startBtn.addEventListener("click", startTimer);
+answersBox.addEventListener("click", answerClick);
+
+function startClick(event) {
+  event.preventDefault();
+  startPrompt.style.display = "none";
+  questionBox.style.display = "block";
+
+  startTimer();
+  showQuestion();
 }
 
-function displayQuestions() {
-  answers = getElement("questionsAnswers");
-  if (questionNumber >= quizQuestions.length) {
-    answers.innerHTML = "<h2>You got "+correctCount+" of "+quizQuestions.length+" questions correct</h2>";
-    getElement("questionsResults").innerHTML = "All done";
-    questionNumber = 0;
-    correctCount = 0;
-    return false;
+const correctWrong = document.createElement("p");
+correctWrong.setAttribute("id", "correctWrong");
+
+function hideResult() {
+  correctWrong.style.display = "none";
+}
+function showResult() {
+  correctWrong.removeAttribute("style");
+}
+
+function answerClick(event) {
+  event.preventDefault();
+
+  if (!event.target.matches("button")) return;
+
+  let userAnswer = event.target.textContent;
+
+  let question = questions[questionInd];
+  let correctAnswer = question.answers[question.correctIndex];
+
+  if (userAnswer === correctAnswer) {
+    correctScore++;
+    correctWrong.textContent = "Correct answer!";
+    questionBox.appendChild(correctWrong);
+    setTimeout(hideResult, 800);
+    showResult();
+  } else {
+    timeTotal = timeTotal - fine;
+    correctWrong.textContent = "Wrong answer!";
+    questionBox.appendChild(correctWrong);
+    setTimeout(hideResult, 800);
+    showResult();
   }
-
-  getElement("questionsResults").innerHTML = "Question "+(questionNumber+1)+" of "+quizQuestions.length;
-
-  questionN = quizQuestions[questionNumber].question;
-  choiceA = quizQuestions[questionNumber].a;
-  choiceB = quizQuestions[questionNumber].b;
-  choiceC = quizQuestions[questionNumber].c;
-
-  answers.innerHTML = "<h3>"+questionN+ "<h3>";
-
-  //   answers.innerHTML += "<button id='choices'>" + choiceA + "</button>";
-  //   answers.innerHTML += "<button id='choices'>" + choiceB + "</button>";
-  //   answers.innerHTML += "<button id='choices'>" + choiceC + "</button>";
-  //   answers.innerHTML +=
-  //     "<button onclick='checkAnswers()'>Submit Answer</button>";
-  answers.innerHTML +=
-    "<label> <input type='radio' name='choices' value='A'> " +
-    choiceA +
-    "</label><br>";
-  answers.innerHTML +=
-    "<label> <input type='radio' name='choices' value='B'> " +
-    choiceB +
-    "</label><br>";
-  answers.innerHTML +=
-    "<label> <input type='radio' name='choices' value='C'> " +
-    choiceC +
-    "</label><br><br>";
-  answers.innerHTML += "<button onclick='checkAnswers()'>Submit Answer</button>";
+  questionInd++;
+  if (questionInd >= questions.length) {
+    clearInterval(startTimer);
+    lastPage();
+  } else {
+    showQuestion();
+  }
 }
-function checkAnswers() {
-  choices = document.getElementsByName("choices");
-  for (var j = 0; j < choices.length; j++) {
-    if (choices[j].checked) {
-      choice = choices[j].value;
+
+function showQuestion() {
+  let curQuestion = questions[questionInd];
+  questionText.textContent = curQuestion.questionText;
+  answersBox.innerHTML = "";
+
+  for (let i = 0; i < curQuestion.answers.length; i++) {
+    const answer = curQuestion.answers[i];
+    const answerBtn = document.createElement("button");
+    answerBtn.setAttribute("class", "answerBtn");
+    answerBtn.textContent = answer;
+    answersBox.appendChild(answerBtn);
+  }
+}
+function startTimer() {
+  setInterval(function () {
+    if (timeTotal > 0) {
+      timer.textContent = `${timeTotal} seconds left`;
+      timeTotal--;
+      score = timeTotal;
+    } else {
+      clearInterval(startTimer);
+      timer.textContent = " Your time is over!";
     }
-  }
-  if (choice === quizQuestions[questionNumber].answer) {
-    correctCount++;
-  }
-  questionNumber++;
-  displayQuestions();
+  }, 1000);
 }
-window.addEventListener("load", displayQuestions);
+
+function lastPage() {
+  let numberCorrectAnswers = document.querySelector("#numberCorrectAnswers");
+  numberCorrectAnswers.textContent = `${correctScore}`;
+  questionBox.style.display = "none";
+  formBox.style.display = "block";
+  timer.style.display = "none";
+  finalScore.textContent = `${score}`;
+}
+
+const startOverBtn = document.querySelector("#startOver");
+function startOver(event) {
+  event.stopPropagation();
+  window.location.replace("index.html");
+}
+startOverBtn.addEventListener("click", startOver);
+
+const submitBtn = document.querySelector("#submit-btn");
+submitBtn.addEventListener("click", submitInitials);
+function submitInitials(event) {
+  event.preventDefault();
+  let initials = document.querySelector("#initials");
+  let initToSend = initials.value;
+  let finalData = {
+    init: initToSend,
+    userscore: finalScore.textContent,
+  };
+
+  let highScores = JSON.parse(window.localStorage.getItem("allScores")) || [];
+
+  highScores.push(finalData);
+
+  localStorage.setItem("allScores", JSON.stringify(highScores));
+}
