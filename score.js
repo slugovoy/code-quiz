a55cc0e91718bcbc4a2ab4a401af5b49ca484ce9
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-11-08 19:54:08 -0700

    added comments

--- score.js
+++ score.js
@@ -1,22 +1,29 @@
+// Variables for Clear button and list of leaders
 let clearBtn = document.querySelector("#clear");
 let clearList = document.querySelector("#orderList");
+
+// Function responsible for pulling data from local storge
 function pullData() {
   let pullScores = JSON.parse(window.localStorage.getItem("allScores")) || [];
 
+  // function for sorting data based on score
   pullScores.sort(function (a, b) {
     return b.userscore - a.userscore;
   });
 
+  // function that assigns data to each li
   pullScores.forEach(function (newScores) {
     let li = document.createElement("li");
     li.textContent = `${newScores.init} : ${newScores.userscore}`;
 
+    // appending li to parent ol
     let olEl = document.querySelector("#orderList");
     olEl.appendChild(li);
   });
 }
 pullData();
 
+// function for clear leaders list and local storage
 function clearHighscores(event) {
   event.stopPropagation();
   window.localStorage.removeItem("allScores");
